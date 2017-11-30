5716da1368ca12da27d806995f76bab54ebec97c
Gabe Bravo <gabecbravo@gmail.com>
2017-11-29 22:10:58 -0500

    Serving static assets.

---
+++ server/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+// import routes
+const notes = require('./routes/notes')
+
+// THIS MUST BE BEFORE THE ROUTES
+app.use(bodyParser.json());
+
+// this is will add the client in deployment
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
+
+// routing middleware >> 2 params >> name of route and routes imported from router
+app.use( '/notes', notes );
+
+// THIS MUST BE AFTER THE ROUTES
+app.use( (err, req, res, next) => {
+  res.json(err);
+});
+
+const port = process.env.PORT;
+app.listen( port, () => {
+  console.log(`Listening on port ${port}`)
+})
