bffcd32c7e6912695575f5236c84e30d867f3735
Gabe Bravo <gabecbravo@gmail.com>
2017-11-29 21:40:50 -0500

    adding login credentials.

--- server/db/index.js
+++ server/db/index.js
@@ -1,8 +1,14 @@
 const { Pool } = require('pg');
-const { user, host, database, password, port } = require('../secrets/db_configuration')
+// const { user, host, database, password, port } = require('../secrets/db_configuration')
 
 const pool = new Pool(
-  { user, host, database, password, port }
+  { 
+    user: 'node_user',
+    host: 'localhost',
+    database: 'notesdb',
+    password: 'password',
+    port: 5432
+  }
 );
 
 module.exports = pool;
